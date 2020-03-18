0041b2e60da443789e580a20a3a97fde72c6bb03
Kiripolszky Károly <k.kiripolszky@mito.hu>
2020-03-18 18:59:39 +0100

    Update Android Gradle plugin to 4.0.0-beta2

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,13 @@ plugins {
 }
 
 buildscript {
-    extra["kotlin_version"] = "1.3.61"
     repositories {
         google()
         jcenter()
         maven(url = "https://maven.fabric.io/public")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.0.0-beta01")
+        classpath("com.android.tools.build:gradle:4.0.0-beta02")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
         classpath("com.google.gms:google-services:4.3.3")
         classpath("io.fabric.tools:gradle:1.31.2")
